67b7f278bff5df5708162cf7aa423ea1061a180d
Darkce <15186743693@163.com>
2022-03-30 16:03:01 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const Web3 = require('web3');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Redis = require('ioredis');
 
 global.web3 = new Web3(
   new HDWalletProvider(
@@ -8,13 +7,5 @@ global.web3 = new Web3(
     'wss://rinkeby.infura.io/ws/v3/b7b864425ef044d8b219ef299a7c9e2c'
   )
 );
-
-global.redis = new Redis({
-  port: 6379,
-  host: '127.0.0.1',
-  family: 4,
-  password: null,
-  db: 4,
-});
 
 exports.PORT = 8098;
